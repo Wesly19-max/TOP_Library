5856fe80e31ef7e79f8c4342a2de05311e3b93b7
Wesly Pardillo <200398940+Wesly19-max@users.noreply.github.com>
2026-05-08 10:32:02 +0800

    Fix grid layout of book cards

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ function addBookToLibrary(bookName,author,numberOfPages,readStatus) {
 
 //display books
 
-addBookToLibrary("Hunger Games","Suzanne Collins",98,false);
-addBookToLibrary("Hunger Games","Suzanne Collins",98,false);
 
 //let display = document.querySelector("p");
 //myLibrary.forEach((index) => {
@@ -34,6 +32,7 @@ addBookToLibrary("Hunger Games","Suzanne Collins",98,false);
 //dialog
 const newBookButton = document.querySelector(".addNewBook");
 const dialog = document.querySelector("dialog");
+const confirmBtn = document.querySelector(".confirmBtn");
 
 
 
@@ -42,7 +41,71 @@ newBookButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
+
+function displayBookCards() {
+  //display user input on the bookcard
+  //for each book object in myLibrary array
+  myLibrary.forEach((index) => {
+    //create new element div (book-card)
+    let div = document.createElement("div");
+    div.classList.add("book-card");
+
+    //fill the div with the properties of objects(e.g.title,author,pages,readstatus)
+    let title = document.createElement("h1");
+    let author = document.createElement("p");
+    let page = document.createElement("p");
+    let removeBtn = document.createElement("button");
+    let readStatusBtn = document.createElement("button");
+
+    title.textContent = `"${index.bookName}"`;
+    author.textContent =  index.author;
+    page.textContent = `${index.numberOfPages} pages`;
+    removeBtn.textContent = "remove";
+    
+    if (index.readStatus === true) {
+      readStatusBtn.textContent = "read";
+    }else {
+      readStatusBtn.textContent = "not read"
+    }
+    
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(page);
+    div.appendChild(removeBtn);
+    div.appendChild(readStatusBtn);
+
+
+    document.querySelector(".mainContent").appendChild(div);
+  });
+};
+
+//display new book card if confirmBtn is clicked
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
+
+  //get user inputs
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const readStatus = document.querySelector("#readstatus").checked;
+
+  //add new book card based on user input
+  addBookToLibrary(title,author,pages,readStatus);
+
+  document.querySelector("form").reset();
+  //display new book card based on library array
+    //delete no books added text
+    const defaultMessage = document.getElementById("default-message");
+    if (defaultMessage) {
+      defaultMessage.remove();
+    }
+  
+    let bookCards = document.querySelectorAll(".book-card");
+
+    for (let i=0;i<bookCards.length;i++) {
+      bookCards[i].remove();
+    }
+    displayBookCards();
+    dialog.close();
 });
 
